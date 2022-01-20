5bd260a626000f6730ef3f473915d7604d754278
Sam Page <sampage6@gmail.com>
2022-01-20 11:49:28 -0600

    got post routes to work

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,10 +1,12 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
-// const postRoutes = require('./postRoutes');
+const gameRoutes = require('./gameRoutes');
+const postRoutes = require('./postRoutes');
 // const commentRoutes = require('./commentRoutes');
 
 router.use('/users', userRoutes);
-// router.use('/posts', postRoutes);
+router.use('/posts', postRoutes);
+router.use('/games', gameRoutes);
 // router.use('/comments', commentRoutes);
 
 module.exports = router;
---
+++ public/js/profile.js
@@ -0,0 +1,56 @@
+const createPost = async (event) => {
+    event.preventDefault();
+
+    const post_body = document.querySelector('#post-body').value.trim();
+    const game_id = window.location.toString().split('/').at(-1)
+
+    if (post_body) {
+        // if (event.target.hasAttribute('data-id')) {
+            // const game_id = event.target.getAttribute('data-id')
+            console.log(game_id)
+        const response = await fetch(`/api/posts/${game_id}`, {
+            method: 'POST',
+            body: JSON.stringify({ post_body }),
+            headers: { 'Content-Type': 'application/json', },
+        });
+
+        if (response.ok) {
+            document.location.replace('/')
+        } else {
+            alert(response.statusText)
+        }
+    }
+    }
+// }
+
+const deletePost = async (event) => {
+    if (event.target.hasAttribute('data-id')) {
+        const id = event.target.getAttribute('data-id')
+        console.log(id)
+
+        const response = await fetch(`/api/posts/${id}`, {
+            method: 'DELETE',
+        });
+
+        if (response.ok) {
+            document.location.replace('/profile')
+        } else {
+            alert(response.statusText)
+        }
+    }
+};
+
+
+
+
+document
+    .querySelector('.create-post')
+    .addEventListener('submit', createPost)
+// const postCreate = document.querySelector('.create-post')
+// postCreate.addEventListener('submit', createPost)
+
+document
+    .querySelector('.delete-post')
+    .addEventListener('click', deletePost)
+
+
