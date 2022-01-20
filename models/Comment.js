7b3b35d26d7d09c90363edcfb91d613c7afa3acd
Jonathan Parker <jprkr21@gmail.com>
2022-01-19 19:49:43 -0600

    editing models index.js

--- models/Comment.js
+++ models/Comment.js
@@ -1,46 +1,49 @@
-const { Model, DataTypes } = require('sequelize');
-const sequelize = require('../config/connection');
+// const { Model, DataTypes } = require('sequelize');
+// const sequelize = require('../config/connection');
 
-class Comment extends Model {}
+// class Comment extends Model {}
 
-Comment.init(
-    {
-        id: {
-            type: DataTypes.INTEGER,
-            primaryKey: true,
-            autoIncrement: true,
-            allowNull: false
-        },
-        post_id: {
-            type: DataTypes.INTEGER,
-            allowNull: false
-        },
-        user_id: {
-            type: DataTypes.INTEGER,
-            allowNull: false
-        },
-        // game_id: {
-        //     type: DataTypes.INTEGER,
-        //     allowNull: true
-        // },
-        comment_body: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-        },
-        date_created: {
-            type: DataTypes.DATE,
-            allowNull: false,
-            defaultValue: DataTypes.NOW,
-          },
-    },
-    {
-        sequelize,
-        timestamps: false,
-        freezeTableName: true,
-        underscored: true,
-        modelName: 'comment',
-    }
-)
+// Comment.init(
+//     {
+//         id: {
+//             type: DataTypes.INTEGER,
+//             primaryKey: true,
+//             autoIncrement: true,
+//             allowNull: false
+//         },
+//         post_id: {
+//             type: DataTypes.INTEGER,
+//             allowNull: false
+//         },
+//         user_id: {
+//             type: DataTypes.INTEGER,
+//             allowNull: false
+//         },
+//         // game_id: {
+//         //     type: DataTypes.INTEGER,
+//         //     allowNull: true
+//         // },
+//         comment_body: {
+//             type: DataTypes.INTEGER,
+//             allowNull: false,
+//         },
+//         date_created: {
+//             type: DataTypes.DATE,
+//             allowNull: false,
+//             defaultValue: DataTypes.NOW,
+//           },
+//     },
+//     {
+//         sequelize,
+//         timestamps: false,
+//         freezeTableName: true,
+//         underscored: true,
+//         modelName: 'comment',
+//     }
+// )
 
 
-module.exports = Comment
+// module.exports = Comment
+
+
+
--- models/index.js
+++ models/index.js
@@ -1,43 +1,166 @@
-const Game = require('./Game')
-const User = require('./User')
-const Post = require('./Post')
-// const Comment = require('./Comment')
+// const Game = require('./Game')
+// const User = require('./User')
+// const Post = require('./Post')
+// // const Comment = require('./Comment')
 
 
 
-Post.belongsTo(User, {
-    foreignKey: 'user_id',
-})
-User.hasMany(Post, {
-    foreignKey: 'user_id',
-    onDelete: 'CASCADE'
-})
+// Post.belongsTo(User, {
+//     foreignKey: 'user_id',
+// })
+// User.hasMany(Post, {
+//     foreignKey: 'user_id',
+//     onDelete: 'CASCADE'
+// })
 
 
-Post.belongsTo(Game, {
-  foreignKey: 'game_id',
-})
+// Post.belongsTo(Game, {
+//   foreignKey: 'game_id',
+// })
 
-Game.hasMany(Post, {
-  foreignKey: 'game_id',
-  })
+// Game.hasMany(Post, {
+//   foreignKey: 'game_id',
+//   })
 
 
-  Post.belongsTo(Game, {
-    foreignKey: 'game_id',
-  })
+//   Post.belongsTo(Game, {
+//     foreignKey: 'game_id',
+//   })
   
-  Game.hasMany(Post, {
-    foreignKey: 'game_id',
-    })
+//   Game.hasMany(Post, {
+//     foreignKey: 'game_id',
+//     })
+
+// module.exports = {Game, User, Post, }
+
+
+
+const sequelize = require('../config/connection');
+const { User, Post, Game, Comment } = require('../models');
+
+// const userData = require('./userData.json');
+// const postData = require('./postData.json');
+const gameData = require('./gameData.json');
+const commentData = require('./commentData.json')
+
+
+const casual = require('casual')
+
+
+const randomUserData =
+    [
+        {
+            username: casual.username,
+            email: casual.email,
+            password: casual.password
+        },
+        {
+            username: casual.username,
+            email: casual.email,
+            password: casual.password
+        },
+        {
+            username: casual.username,
+            email: casual.email,
+            password: casual.password
+        },
+        {
+            username: casual.username,
+            email: casual.email,
+            password: casual.password
+        }
+    ]
+
+
+const randomPostData =
+    [
+        {
+            post_body: casual.sentence,
+            date_created: casual.date(format = 'YYYY-MM-DD')
+        },
+        {
+            post_body: casual.sentence,
+            date_created: casual.date(format = 'YYYY-MM-DD')
+        },
+        {
+            post_body: casual.sentence,
+            date_created: casual.date(format = 'YYYY-MM-DD')
+        },
+        {
+            post_body: casual.sentence,
+            date_created: casual.date(format = 'YYYY-MM-DD')
+        }
+    ]
+
+
+// const randomCommentData = [
+//     {
+//         comment_body: casual.sentence,
+//         date_created: casual.date(format = 'YYYY-MM-DD')
+//     },
+//     {
+//         comment_body: casual.sentence,
+//         date_created: casual.date(format = 'YYYY-MM-DD')
+//     },
+//     {
+//         comment_body: casual.sentence,
+//         date_created: casual.date(format = 'YYYY-MM-DD')
+//     },
+//     {
+//         comment_body: casual.sentence,
+//         date_created: casual.date(format = 'YYYY-MM-DD')
+//     }
+// ]
 
 
 
 
 
+const seedDatabase = async () => {
+    await sequelize.sync({ force: true });
+    try {
+        const users = await User.bulkCreate(randomUserData, {
+            individualHooks: true,
+            returning: true,
+        });
 
+        const games = await Game.bulkCreate(gameData, {
+            // individualHooks: true,
+            returning: true,
+        });
 
+        
+        for (const post of randomPostData) {
+            await Post.create({
+                ...post,
+                user_id: users[Math.floor(Math.random() * users.length)].id,
+                game_id: games[Math.floor(Math.random() * games.length)].id,
 
+            });
+          
 
+        //////////////////////////////////////////////////////////////////////////////////////////
 
-module.exports = {Game, User, Post, }
+        // for (const comment of commentData) {
+        //     await Comment.create({
+        //         ...comment,
+        //         user_id: users[Math.floor(Math.random() * users.length)].id,
+        //         post_id: post[Math.floor(Math.random() * post.length)].id,
+        //     });
+        // }
+    
+        // const comment = await Comment.bulkCreate(commentData, {
+        //     returning: true,
+        //     user_id: users[Math.floor(Math.random() * users.length)].id,
+        //     post_id: post[Math.floor(Math.random() * post.length)].id,
+        // })
+    }
+        //////////////////////////////////////////////////////////////////////////////////////////
+    }
+    catch (error) {
+        console.log(error)
+    }
+    process.exit(0);
+};
+
+seedDatabase();
