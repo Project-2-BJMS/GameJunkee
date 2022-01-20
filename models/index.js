6ab541a9bb590bcb4f4a8e69c882853df89c83eb
bseton11-YJKnqQ <bseton11@gmail.com>
2022-01-19 20:36:56 -0600

    quick modification

--- models/index.js
+++ models/index.js
@@ -1,166 +1,55 @@
-// const Game = require('./Game')
-// const User = require('./User')
-// const Post = require('./Post')
-// // const Comment = require('./Comment')
+const Game = require('./Game')
+const User = require('./User')
+const Post = require('./Post')
+const Comment = require('./Comment')
 
 
 
-// Post.belongsTo(User, {
-//     foreignKey: 'user_id',
-// })
-// User.hasMany(Post, {
-//     foreignKey: 'user_id',
-//     onDelete: 'CASCADE'
-// })
+Post.belongsTo(User, {
+    foreignKey: 'user_id',
+})
+User.hasMany(Post, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+})
 
 
-// Post.belongsTo(Game, {
-//   foreignKey: 'game_id',
-// })
+Post.belongsTo(Game, {
+  foreignKey: 'game_id',
+})
 
-// Game.hasMany(Post, {
-//   foreignKey: 'game_id',
-//   })
-
-
-//   Post.belongsTo(Game, {
-//     foreignKey: 'game_id',
-//   })
-  
-//   Game.hasMany(Post, {
-//     foreignKey: 'game_id',
-//     })
-
-// module.exports = {Game, User, Post, }
+Game.hasMany(Post, {
+  foreignKey: 'game_id',
+  })
 
 
+//////////////////////////////////////////////////////////////////////////////////////////
 
-const sequelize = require('../config/connection');
-const { User, Post, Game, Comment } = require('../models');
+//comment to user is 1 to 1 with fkey defined in source model
+Comment.belongsTo(User, {
+  foreignKey: 'user_id',
+})
 
-// const userData = require('./userData.json');
-// const postData = require('./postData.json');
-const gameData = require('./gameData.json');
-const commentData = require('./commentData.json')
+//user to comment is a 1 to many with fkey defined in target model
+User.hasMany(Comment, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE'
+})
 
+//comment to post is a 1 to 1 with fkey defined in source model
+Comment.belongsTo(Post, {
+  foreignKey: 'post_id',
+})
 
-const casual = require('casual')
-
-
-const randomUserData =
-    [
-        {
-            username: casual.username,
-            email: casual.email,
-            password: casual.password
-        },
-        {
-            username: casual.username,
-            email: casual.email,
-            password: casual.password
-        },
-        {
-            username: casual.username,
-            email: casual.email,
-            password: casual.password
-        },
-        {
-            username: casual.username,
-            email: casual.email,
-            password: casual.password
-        }
-    ]
-
-
-const randomPostData =
-    [
-        {
-            post_body: casual.sentence,
-            date_created: casual.date(format = 'YYYY-MM-DD')
-        },
-        {
-            post_body: casual.sentence,
-            date_created: casual.date(format = 'YYYY-MM-DD')
-        },
-        {
-            post_body: casual.sentence,
-            date_created: casual.date(format = 'YYYY-MM-DD')
-        },
-        {
-            post_body: casual.sentence,
-            date_created: casual.date(format = 'YYYY-MM-DD')
-        }
-    ]
-
-
-// const randomCommentData = [
-//     {
-//         comment_body: casual.sentence,
-//         date_created: casual.date(format = 'YYYY-MM-DD')
-//     },
-//     {
-//         comment_body: casual.sentence,
-//         date_created: casual.date(format = 'YYYY-MM-DD')
-//     },
-//     {
-//         comment_body: casual.sentence,
-//         date_created: casual.date(format = 'YYYY-MM-DD')
-//     },
-//     {
-//         comment_body: casual.sentence,
-//         date_created: casual.date(format = 'YYYY-MM-DD')
-//     }
-// ]
+// post to comment is a 1 to many fkey defined in target model 
+Post.hasMany(Comment, {
+  foreignKey: 'post_id',
+  onDelete: 'CASCADE'
+})
+//////////////////////////////////////////////////////////////////////////////////////////
 
 
 
 
 
-const seedDatabase = async () => {
-    await sequelize.sync({ force: true });
-    try {
-        const users = await User.bulkCreate(randomUserData, {
-            individualHooks: true,
-            returning: true,
-        });
-
-        const games = await Game.bulkCreate(gameData, {
-            // individualHooks: true,
-            returning: true,
-        });
-
-        
-        for (const post of randomPostData) {
-            await Post.create({
-                ...post,
-                user_id: users[Math.floor(Math.random() * users.length)].id,
-                game_id: games[Math.floor(Math.random() * games.length)].id,
-
-            });
-          
-
-        //////////////////////////////////////////////////////////////////////////////////////////
-
-        // for (const comment of commentData) {
-        //     await Comment.create({
-        //         ...comment,
-        //         user_id: users[Math.floor(Math.random() * users.length)].id,
-        //         post_id: post[Math.floor(Math.random() * post.length)].id,
-        //     });
-        // }
-    
-        // const comment = await Comment.bulkCreate(commentData, {
-        //     returning: true,
-        //     user_id: users[Math.floor(Math.random() * users.length)].id,
-        //     post_id: post[Math.floor(Math.random() * post.length)].id,
-        // })
-    }
-        //////////////////////////////////////////////////////////////////////////////////////////
-    }
-    catch (error) {
-        console.log(error)
-    }
-    process.exit(0);
-};
-
-seedDatabase();
+module.exports = {Game, User, Post, Comment}
