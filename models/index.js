7bcd40ae5374e5ffff3a506ac2b21b5d8f184989
monstermiles <miles.emmitt.moss@gmail.com>
2022-01-21 19:24:09 -0600

    deleted some comments

--- models/index.js
+++ models/index.js
@@ -1,67 +1,3 @@
-// const Game = require('./Game')
-// const User = require('./User')
-// const Post = require('./Post')
-// const Comment = require('./Comment')
-
-
-
-// Post.belongsTo(User, {
-//     foreignKey: 'user_id',
-// })
-// User.hasMany(Post, {
-//     foreignKey: 'user_id',
-//     onDelete: 'CASCADE'
-// })
-
-// Game.belongsTo(User, {
-//   foreignKey: 'user_id',
-// })
-
-// User.hasMany(Game, {
-//   foreignKey: 'user_id',
-//   })
-
-
-// Post.belongsTo(Game, {
-//   foreignKey: 'game_id',
-// })
-
-// Game.hasMany(Post, {
-//   foreignKey: 'game_id',
-//   })
-
-
-// //////////////////////////////////////////////////////////////////////////////////////////
-
-// //comment to user is 1 to 1 with fkey defined in source model
-// Comment.belongsTo(User, {
-//   foreignKey: 'user_id',
-// })
-
-// //user to comment is a 1 to many with fkey defined in target model
-// User.hasMany(Comment, {
-//   foreignKey: 'user_id',
-//   onDelete: 'CASCADE'
-// })
-
-// //comment to post is a 1 to 1 with fkey defined in source model
-// Comment.belongsTo(Post, {
-//   foreignKey: 'post_id',
-// })
-
-// // post to comment is a 1 to many fkey defined in target model 
-// Post.hasMany(Comment, {
-//   foreignKey: 'post_id',
-//   onDelete: 'CASCADE'
-// })
-// //////////////////////////////////////////////////////////////////////////////////////////
-
-
-
-
-
-// module.exports = {Game, User, Post, Comment}
-
 const Game = require('./Game')
 const User = require('./User')
 const Post = require('./Post')
@@ -70,11 +6,11 @@ const Comment = require('./Comment')
 
 
 Post.belongsTo(User, {
-    foreignKey: 'user_id',
+  foreignKey: 'user_id',
 })
 User.hasMany(Post, {
-    foreignKey: 'user_id',
-    onDelete: 'CASCADE'
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE'
 })
 
 Game.belongsTo(User, {
@@ -83,8 +19,7 @@ Game.belongsTo(User, {
 User.hasMany(Game, {
   foreignKey: 'user_id',
   onDelete: 'CASCADE'
-  })
-
+})
 
 
 Post.belongsTo(Game, {
@@ -95,39 +30,35 @@ Post.belongsTo(Game, {
 Game.hasMany(Post, {
   foreignKey: 'game_id',
   onDelete: 'CASCADE'
-  })
+})
 
 
-//////////////////////////////////////////////////////////////////////////////////////////
-
-//comment to user is 1 to 1 with fkey defined in source model
 Comment.belongsTo(User, {
   foreignKey: 'user_id',
 })
 
-//user to comment is a 1 to many with fkey defined in target model
+
 User.hasMany(Comment, {
   foreignKey: 'user_id',
   onDelete: 'CASCADE'
 })
 
-//comment to post is a 1 to 1 with fkey defined in source model
+
 Comment.belongsTo(Post, {
   foreignKey: 'post_id',
 })
 
-// post to comment is a 1 to many fkey defined in target model 
+
 Post.hasMany(Comment, {
   foreignKey: 'post_id',
   onDelete: 'CASCADE'
 })
-//////////////////////////////////////////////////////////////////////////////////////////
 
 
 
 
 
-module.exports = {Game, User, Post, Comment}
+module.exports = { Game, User, Post, Comment }
 
 
 
